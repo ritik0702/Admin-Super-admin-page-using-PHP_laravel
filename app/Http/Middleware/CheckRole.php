3c811dba960c9b938a84199d5d8ef41717039f78
ritik0702 <74063490+ritik0702@users.noreply.github.com>
2024-07-02 23:42:11 +0530

    Add files via upload

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,21 @@
+<?php
+
+// app/Http/Middleware/CheckRole.php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckRole
+{
+    public function handle(Request $request, Closure $next, $role)
+    {
+        if (!Auth::check() || Auth::user()->role != $role) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        return $next($request);
+    }
+}
